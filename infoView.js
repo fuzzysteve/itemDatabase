6d50f8b0f931af271e42a2f1f51d369c3b7a74de
Steve Anderson <steve@fuzzwork.co.uk>
2016-03-04 22:43:15 +0100

    Added some handling for inverse percentages, and a set of helper functions for rounding to percentages

--- infoView.js
+++ infoView.js
@@ -426,6 +426,13 @@ function attributes(attributeTree) {
             if (attributeTypes[key].displayunit == 's') {
                 itemdata[key].value=itemdata[key].value/1000;
             }
+            if (attributeTypes[key].unitname == 'Modifier Percent') {
+                itemdata[key].value=Math.round10(((itemdata[key].value-1)*100),-2);
+            }
+            if (attributeTypes[key].unitname == 'Inversed Modifier Percent') {
+                itemdata[key].value=Math.round10(((1-itemdata[key].value)*100),-2);
+            }
+
             myObj[attributeTypes[key].displayname]=itemdata[key].value+" "+attributeTypes[key].displayunit;
             displayTree[attributeTypes[key].categoryname].push(myObj);
         } else {
@@ -584,3 +591,52 @@ function formatDogma(attributeID,value) {
 
 
 var activityName={'1':'Manufacturing','3':'TE Research','4':'ME research','5':'Copying','7':'Reverse Engineering','8':'Invention'};
+
+
+(function() {
+  /**
+   * Decimal adjustment of a number.
+   *
+   * @param {String}  type  The type of adjustment.
+   * @param {Number}  value The number.
+   * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
+   * @returns {Number} The adjusted value.
+   */
+  function decimalAdjust(type, value, exp) {
+    // If the exp is undefined or zero...
+    if (typeof exp === 'undefined' || +exp === 0) {
+      return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // If the value is not a number or the exp is not an integer...
+    if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+      return NaN;
+    }
+    // Shift
+    value = value.toString().split('e');
+    value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+    // Shift back
+    value = value.toString().split('e');
+    return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+  }
+
+  // Decimal round
+  if (!Math.round10) {
+    Math.round10 = function(value, exp) {
+      return decimalAdjust('round', value, exp);
+    };
+  }
+  // Decimal floor
+  if (!Math.floor10) {
+    Math.floor10 = function(value, exp) {
+      return decimalAdjust('floor', value, exp);
+    };
+  }
+  // Decimal ceil
+  if (!Math.ceil10) {
+    Math.ceil10 = function(value, exp) {
+      return decimalAdjust('ceil', value, exp);
+    };
+  }
+})();
